9bc6f0627744a43525264e9023111de44652b0cd
joannec <j.connor@kainos.com>
2016-08-14 22:26:40 +0100

    added new image, hero div html, javascripts, css

---
+++ javascripts/main_js.js
@@ -0,0 +1,30 @@
+//jQuery for hero div on home page
+
+jQuery(document).ready(function($){
+
+// fade the header in 
+	 jQuery('#hero_header').fadeIn(10000);
+ // Defining a function to set size for #hero 
+	function fullscreen(){
+		jQuery('#hero').css({
+            width: jQuery(window).width(),
+            height: jQuery(window).height()
+        });
+	}
+  
+	fullscreen();
+
+  // Run the function in case of window resize
+  jQuery(window).resize(function() {
+       fullscreen();         
+    });
+
+
+    
+ 
+        
+    	
+	
+	
+
+});
