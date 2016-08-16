f79ae31ce3491425ad831874567687110287c49d
John <j.kavanagh@kainos.com>
2016-08-16 13:14:15 +0100

    Updated Nav  Bar and CSS Johnk

--- javascripts/jquery.bxslider.js
+++ javascripts/jquery.bxslider.js
@@ -86,6 +86,14 @@
 		onSlidePrev: function() {},
 		onSliderResize: function() {}
 	}
+	function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 	$.fn.bxSlider = function(options){
 
